72f8b63fd72842a8ed06ce6d391c47fed48d7723
yfbahtiar <yusuf2bahtiar@gmail.com>
2023-09-19 15:52:32 +0700

    upd:bg login

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,20 +1,28 @@
 "use client";
 
 import {
+  Button,
+  Checkbox,
   Flex,
-  Box,
-  Stack,
-  Heading,
   Text,
-  Container,
-  Center,Spinner, SimpleGrid,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Image,
+  Center,
+  Spinner,
+  Box,
   useColorModeValue,
+  Container,
+  SimpleGrid,
 } from "@chakra-ui/react";
-import { FaRegCircleUser } from "react-icons/fa6";
+// import { FaRegCircleUser } from "react-icons/fa6";
 import { useAuth } from "../components/AuthContext";
-import Bg_Stp from "../components/img/bg-stp.jpg";
+// import Bg_Stp from "../components/img/bg-stp.jpg";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+// import { useState } from "react";
 
 export const metadata = {
   title: process.env.APP_NAME,
@@ -29,7 +37,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   if (user !== null && user !== 401) {
     router.push("/");
   }
-  
+
   return (
     <main>
       {loadingValidation ? (
@@ -37,51 +45,51 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Spinner className="spinner" size="xl" color="blue.500" />
         </Center>
       ) : (
-        <Box
-          position={"relative"}
-          // bgImage={`url('${Bg_Stp.src}')`}
-          backgroundImage="url('/img/bg-stp.jpg')"
-          backgroundSize="cover"
-          backgroundRepeat="no-repeat"
-          backgroundPosition="center"
-          // w="100vw"
-          h="100vh"
-        >
-          <Container
-            as={SimpleGrid}
-            maxW={"7xl"}
-            columns={{ base: 1, md: 2 }}
-            spacing={{ base: 10, lg: 32 }}
-            py={{ base: 10, sm: 20, lg: 32 }}
-          >
-            <Stack
-              // bg={"gray.50"}
-              bgColor="rgba(231, 231, 231, 0.5)"
-              rounded={"xl"}
-              p={{ base: 4, sm: 6, md: 8 }}
-              spacing={{ base: 8 }}
-              justifyContent="right"
-              maxW="md"
+        <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+          <Flex p={8} flex={1} align={"center"} justify={"center"}>
+            <Container
+              as={SimpleGrid}
+              maxW={"7xl"}
+              columns={{ base: 1, md: 2 }}
+              spacing={{ base: 10, lg: 32 }}
+              py={{ base: 10, sm: 20, lg: 32 }}
             >
-              <Stack align={"center"}>
-                {/* <FaRegCircleUser fontSize="3.5rem" /> */}
-                <Heading fontSize={"4xl"}>SELAMAT DATANG</Heading>
-                <Text fontSize={"lg"} color={"gray.600"}>
-                  Silahkan login terlebih dahulu
-                </Text>
-              </Stack>
-              <Box
-                rounded={"lg"}
-                // w="52vh"
-                bg={useColorModeValue("white", "gray.700")}
-                boxShadow={"lg"}
-                p={8}
+              <Stack
+                // bg={"gray.50"}
+                bgColor="rgba(231, 231, 231, 0.5)"
+                rounded={"xl"}
+                p={{ base: 4, sm: 6, md: 8 }}
+                spacing={{ base: 8 }}
+                justifyContent="right"
+                maxW="md"
               >
-                {children}
-              </Box>
-            </Stack>
-          </Container>
-        </Box>
+                <Stack align={"center"}>
+                  {/* <FaRegCircleUser fontSize="3.5rem" /> */}
+                  <Heading fontSize={"4xl"}>SELAMAT DATANG</Heading>
+                  <Text fontSize={"lg"} color={"gray.600"}>
+                    Silahkan login terlebih dahulu
+                  </Text>
+                </Stack>
+                <Box
+                  rounded={"lg"}
+                  // w="52vh"
+                  bg={useColorModeValue("white", "gray.700")}
+                  boxShadow={"lg"}
+                  p={8}
+                >
+                  {children}
+                </Box>
+              </Stack>
+            </Container>
+          </Flex>
+          <Flex flex={1}>
+            <Image
+              alt={"Login Image"}
+              objectFit={"cover"}
+              src={"/img/login-stp.svg"}
+            />
+          </Flex>
+        </Stack>
       )}
     </main>
   );
