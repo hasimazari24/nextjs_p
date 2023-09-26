417c7df451b593718abf1075bc48b2efbcb5ebdf
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-09-26 14:42:36 +0700

    add data tenant login as tenant

--- src/app/(secure)/tenant/page.tsx
+++ src/app/(secure)/tenant/page.tsx
@@ -6,11 +6,23 @@ import { useEffect, useState, useContext } from "react";
 import ModalEdit from "./modal-edit";
 import ModalDetail from "./modal-detail";
 import {
+  chakra,
+  Container,
+  VStack,
+  SimpleGrid,
+  StackDivider,
+  VisuallyHidden,
+  List,
+  ListItem,
   Button,
   Center,
   HStack,
   Spinner,
-  Text,useColorModeValue,Image, Avatar, Stack,
+  Text,
+  useColorModeValue,
+  Image,
+  Avatar,Td,Tr,Tbody,Table,
+  Stack,
   Heading,
   Flex,
   Menu,
@@ -19,6 +31,8 @@ import {
   MenuItem,
   MenuList,
   Box,
+  Divider,
+  IconButton,
 } from "@chakra-ui/react";
 import ConfirmationModal from "../../components/modal/modal-confirm";
 import ModalNotif from "../../components/modal/modal-notif";
@@ -34,7 +48,12 @@ import { useRouter } from "next/navigation";
 import { GrMoreVertical } from "react-icons/gr";
 import { SiMicrosoftteams } from "react-icons/si";
 import { BiLinkExternal } from "react-icons/bi";
+import { CiGlobe } from "react-icons/ci";
 import { axiosCustom } from "@/app/api/axios";
+import Link from "next/link";
+import { FiFacebook } from "react-icons/fi";
+import { TbWorldWww } from "react-icons/tb";
+import { AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
 
 interface DataItem {
   id: string;
@@ -303,64 +322,277 @@ export default function page() {
           >
             <Heading fontSize={"2xl"}>DATA TENANT</Heading>
             <HStack>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  bgColor="green.100"
+                  _hover={{
+                    bg: "green.200",
+                  }}
+                  // color="white"
+                  title="More ..."
+                  // onClick={() => handleDetail(rowData)}
+                  key="dataDetail"
+                  size="sm"
+                >
+                  <GrMoreVertical />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                    <BiLinkExternal />
+                    &nbsp; Lihat Situs
+                  </MenuItem>
+                  <MenuItem
+                  // onClick={() =>
+                  //   router.push(`/tenant/catalog?id=${rowData.id}`)
+                  // }
+                  >
+                    <HamburgerIcon />
+                    &nbsp; Catalog Tenant
+                  </MenuItem>
+                  <MenuItem
+                  // onClick={() => router.push(`/tenant/team?id=${rowData.id}`)}
+                  >
+                    <SiMicrosoftteams />
+                    &nbsp; Team Tenant
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              &nbsp;
               <Button
-                bgColor="green.300"
+                bgColor="blue.100"
                 _hover={{
-                  bg: "green.400",
+                  bg: "blue.200",
                 }}
+                // color="white"
+                title="Edit Data"
+                // onClick={() => handleEdit(rowData)}
+                fontSize="14px"
+                fontWeight="230"
+                key="editData"
+                size="sm"
               >
-                <AddIcon />
-                &nbsp;Edit
-              </Button>
-              <Button
-                bgColor="red.300"
-                _hover={{
-                  bg: "red.400",
-                }}
-              >
-                <AddIcon />
-                &nbsp;Edit
+                <EditIcon /> &nbsp; Edit Data Tenant
               </Button>
             </HStack>
           </Flex>
 
-          <Box
-            maxW={"7xl"}
-            w={"full"}
-            bg={useColorModeValue("white", "gray.800")}
-            boxShadow={"2xl"}
-            rounded={"md"}
-            overflow={"hidden"}
-          >
-            <Image
-              h={"280px"}
-              w={"full"}
-              src={
-                "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-              }
-              objectFit="cover"
-              alt="#"
+          <Container maxW={"7xl"} pl="0" pr="0">
+            <SimpleGrid
+              columns={{ base: 1, lg: 2 }}
+              spacing={{ base: 8, md: 10 }}
+            >
+              <VStack>
+                <Image
+                  rounded={"md"}
+                  alt={"product image"}
+                  src={
+                    "https://images.unsplash.com/photo-1596516109370-29001ec8ec36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE1MDl8MHwxfGFsbHx8fHx8fHx8fDE2Mzg5MzY2MzE&ixlib=rb-1.2.1&q=80&w=1080"
+                  }
+                  fit={"cover"}
+                  align={"center"}
+                  w={"full"}
+                  h={{ base: "150px", sm: "300px", lg: "300px" }}
+                />
+                <Flex
+                  justify={"center"}
+                  mt={{ base: "-50px", sm: "-100", lg: "-100" }}
+                >
+                  <Avatar
+                    h={{ base: "100px", sm: "200px", lg: "200px" }}
+                    w="100%"
+                    src={
+                      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+                    }
+                    css={{
+                      border: "5px solid white",
+                    }}
+                  />
+                </Flex>
+              </VStack>
+
+              <Stack spacing={{ base: 4, md: 6 }}>
+                <Box as={"header"}>
+                  <Heading fontSize="2xl">IMPULS.ID</Heading>
+                  <Text
+                    color={useColorModeValue("gray.900", "gray.400")}
+                    fontWeight={300}
+                    fontSize={"x5"}
+                  >
+                    Level Tenant : <b>Inkubasi</b>
+                  </Text>
+                </Box>
+
+                <Stack
+                  spacing={{ base: 4, sm: 6 }}
+                  direction={"column"}
+                  divider={
+                    <StackDivider
+                      borderColor={useColorModeValue("gray.200", "gray.600")}
+                    />
+                  }
+                >
+                  <Text
+                    color={useColorModeValue("gray.500", "gray.400")}
+                    fontSize={"xl"}
+                    // fontWeight={"300"}
+                  >
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    diam nonumy eirmod tempor invidunt ut labore
+                  </Text>
+                  <Box>
+                    <Text
+                      fontSize={{ base: "16px", lg: "18px" }}
+                      color={useColorModeValue("yellow.500", "yellow.300")}
+                      fontWeight={"500"}
+                      textTransform={"uppercase"}
+                      mb={"4"}
+                    >
+                      Tenant Details
+                    </Text>
+
+                    <SimpleGrid spacing={10}>
+                      <Table pt="0">
+                        <Tbody>
+                          <Tr key={2} pb="3">
+                            <Td width="20%" pr="0" pl="0" pt="0">
+                              Founder
+                            </Td>
+                            <Td
+                              width="5%"
+                              pr="0"
+                              pl="0"
+                              pt="0"
+                              textAlign="center"
+                            >
+                              :
+                            </Td>
+                            <Td width="75%" pr="0" pt="0" pl="0">
+                              Founder Tenant
+                            </Td>
+                          </Tr>
+
+                          <Tr key={1}>
+                            <Td width="20%" pr="0" pl="0">
+                              Alamat
+                            </Td>
+                            <Td width="5%" textAlign="center" pr="0" pl="0">
+                              :
+                            </Td>
+                            <Td width="75%" pr="0" pl="0">
+                              Surakarta, Indonesia, Jawa Tengah, Timur Raya,
+                              564522
+                            </Td>
+                            {/* Tambahkan kolom lainnya sesuai kebutuhan */}
+                          </Tr>
+
+                          <Tr key={4} pb="3">
+                            <Td width="20%" pr="0" pl="0">
+                              E-mail
+                            </Td>
+                            <Td width="5%" pr="0" pl="0" textAlign="center">
+                              :
+                            </Td>
+                            <Td width="75%" pr="0" pl="0">
+                              impuls@tech.id
+                            </Td>
+                          </Tr>
+                          <Tr key={5} borderBottom={"hidden"}>
+                            <Td width="20%" pr="0" pl="0">
+                              Kontak
+                            </Td>
+                            <Td width="5%" pr="0" pl="0" textAlign="center">
+                              :
+                            </Td>
+                            <Td width="75%" pr="0" pl="0">
+                              089732423882
+                            </Td>
+                          </Tr>
+                        </Tbody>
+                      </Table>
+                    </SimpleGrid>
+                  </Box>
+                </Stack>
+              </Stack>
+            </SimpleGrid>
+
+            <Divider
+              pt="4"
+              mb="4"
+              color={useColorModeValue("gray.200", "gray.600")}
+              borderColor={useColorModeValue("gray.200", "gray.600")}
             />
-            <Flex justify={"center"} mt={-28}>
-              <Avatar
-                size={"x2"}
-                src={
-                  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                }
-                css={{
-                  border: "10px solid white",
-                }}
-              />
-            </Flex>
-            <Stack spacing={2} align={"center"} mb={5} p="4">
-              <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
-                CV. IMPULS.ID
-              </Heading>
-              <Text >
-                Pengembangan software dan digitalisasi pendidikan
-              </Text>
+
+            <Stack spacing={{ base: 4, md: 6 }} pb="3">
+              <Box>
+                <Flex
+                  justifyContent={"space-between"}
+                  direction={["column", "row"]}
+                  mb={"4"}
+                >
+                  <Text
+                    fontSize={{ base: "16px", lg: "18px" }}
+                    color={useColorModeValue("yellow.500", "yellow.300")}
+                    fontWeight={"500"}
+                    textTransform={"uppercase"}
+                    mb={"2"}
+                  >
+                    Social Links
+                  </Text>
+
+                  <Button
+                    bgColor="blue.100"
+                    _hover={{
+                      bg: "blue.200",
+                    }}
+                    // color="white"
+                    title="Edit Social Links"
+                    // onClick={() => handleEdit(rowData)}
+                    key="editSocial"
+                    size="sm"
+                    fontSize="14px"
+                    fontWeight="230"
+                  >
+                    <EditIcon /> &nbsp; Edit Social Links
+                  </Button>
+                </Flex>
+
+                <Stack direction={["column", "row"]}>
+                  <Link href="impuls.id">
+                    <HStack alignItems={"center"} pr="3">
+                      <IconButton
+                        colorScheme="blue"
+                        aria-label="web"
+                        icon={<TbWorldWww size="xs" />}
+                      />
+                      <Text fontSize="16px">impuls.id</Text>
+                    </HStack>
+                  </Link>
+                  <Link href="impuls.id">
+                    <HStack alignItems={"center"} pr="3">
+                      <IconButton
+                        colorScheme="facebook"
+                        aria-label="web"
+                        icon={<AiOutlineFacebook size="xs" />}
+                      />
+                      <Text fontSize="16px">impuls.id</Text>
+                    </HStack>
+                  </Link>
+
+                  <Link href="impuls.id">
+                    <HStack alignItems={"center"} pr="3">
+                      <IconButton
+                        colorScheme="linkedin"
+                        aria-label="web"
+                        icon={<AiOutlineLinkedin size="xs" />}
+                      />
+                      <Text fontSize="16px">impuls.id</Text>
+                    </HStack>
+                  </Link>
+                </Stack>
+              </Box>
             </Stack>
-          </Box>
+          </Container>
 
           <Flex
             justifyContent={"space-between"}
