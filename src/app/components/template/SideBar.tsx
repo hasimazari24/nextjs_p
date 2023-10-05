bdcc361cbb40e5546e13c65457e5cdf928c82c49
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-10-05 12:50:44 +0700

    add loading reload page, add breadcrumbs

--- src/app/components/template/SideBar.tsx
+++ src/app/components/template/SideBar.tsx
@@ -10,11 +10,14 @@ import {
   BoxProps,
   FlexProps,
   Img,
+  Spinner,
 } from "@chakra-ui/react";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { BsBuilding, BsPeople } from "react-icons/bs";
 import { IconType } from "react-icons";
 import { useAuth } from "../utils/AuthContext";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 interface LinkItemProps {
   name: string;
@@ -44,8 +47,21 @@ const LinkItems_tenant: Array<LinkItemProps> = [
 ];
 
 const NavItem = ({ icon, children, link, ...rest }: NavItemProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (link === pathname) setIsLoading(false);
+  }, [pathname]);
+
   return (
-    <NextLink href={link} passHref>
+    <NextLink
+      href={link}
+      passHref
+      onClick={() => {
+        if (link !== pathname) setIsLoading(true);
+      }}
+    >
       {/* <a> */}
       <Flex
         align="center"
@@ -70,7 +86,10 @@ const NavItem = ({ icon, children, link, ...rest }: NavItemProps) => {
             as={icon}
           />
         )}
-        {children}
+        {children} &nbsp;
+        {isLoading ? (
+          <Spinner className="spinner" color="blue.500" />
+        ) : null}
       </Flex>
       {/* </a> */}
     </NextLink>
@@ -111,7 +130,7 @@ const Sidebar = ({ onClose, ...rest }: SidebarProps) => {
       {...rest}
     >
       <Flex h="16" alignItems="center" mx="8" justifyContent="center">
-        <Img src="/img/LOGO-STP.png" h="35px" />
+        <Img src="/img/siteman-primary.png" h="35px" />
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
 
