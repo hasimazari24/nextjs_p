bd2f59e6ce3540105101544b6ceeb5f1ede3a688
yfbahtiar <yusuf2bahtiar@gmail.com>
2023-10-25 12:36:53 +0700

    upd: bulk delete assets

--- src/app/(secure)/assets/page.tsx
+++ src/app/(secure)/assets/page.tsx
@@ -22,7 +22,13 @@ import {
 } from "@chakra-ui/react";
 import ConfirmationModal from "../../components/modal/modal-confirm";
 import ModalNotif from "../../components/modal/modal-notif";
-import { AddIcon, DeleteIcon, EditIcon, HamburgerIcon, ViewIcon } from "@chakra-ui/icons";
+import {
+  AddIcon,
+  DeleteIcon,
+  EditIcon,
+  HamburgerIcon,
+  ViewIcon,
+} from "@chakra-ui/icons";
 import { useRouter } from "next/navigation";
 import { GrMoreVertical, GrShareOption, GrTrophy } from "react-icons/gr";
 import { SiMicrosoftteams } from "react-icons/si";
@@ -57,205 +63,202 @@ interface UserLog {
 }
 
 function page() {
-    const { user } = useAuth();
-    let getUser: UserLog | null = null; // Inisialisasikan getUser di sini
+  const { user } = useAuth();
+  let getUser: UserLog | null = null; // Inisialisasikan getUser di sini
 
-    if (user !== null && user !== 401) {
-      getUser = user; // Setel nilai getUser jika user ada
+  if (user !== null && user !== 401) {
+    getUser = user; // Setel nilai getUser jika user ada
+  }
+
+  let assetsFeatures: any | null | undefined = null; // Inisialisasikan fitur pada menunya
+  let allMenu: any | null = null;
+  if (getUser !== null) {
+    // ambil permission sesuai login role
+    assetsFeatures = permissions[getUser.role]?.features.find(
+      (feature) => feature.menu === "assets",
+    );
+    //ambil permision features all menu (hanya utk admin)
+    allMenu = permissions[getUser.role]?.features.find(
+      (feature) => feature.menu === "allmenu",
+    );
+  }
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isModalNotif, setModalNotif] = useState(false);
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const handleShowMessage = (msg: string, err: boolean) => {
+    setMessage(msg);
+    setIsError(err);
+    setModalNotif(true);
+  };
+
+  const hidenCols = ["id", "url"];
+
+  const avatarExt = (type: any) => {
+    switch (type) {
+      case "JPG" || "JPEG":
+        return BsFiletypeJpg;
+      case "PNG":
+        return BsFiletypePng;
+      case "GIF":
+        return BsFiletypeGif;
+      case "SVG":
+        return BsFiletypeSvg;
+      case "PDF":
+        return BsFiletypePdf;
+      default:
+        return AiOutlineFileUnknown;
     }
+  };
 
-    let assetsFeatures: any | null | undefined = null; // Inisialisasikan fitur pada menunya
-    let allMenu: any | null = null;
-    if (getUser !== null) {
-      // ambil permission sesuai login role
-      assetsFeatures = permissions[getUser.role]?.features.find(
-        (feature) => feature.menu === "assets",
-      );
-      //ambil permision features all menu (hanya utk admin)
-      allMenu = permissions[getUser.role]?.features.find(
-        (feature) => feature.menu === "allmenu",
-      );
+  const columns: ReadonlyArray<Column<DataItem>> = [
+    {
+      id: "selection",
+      Header: ({ getToggleAllPageRowsSelectedProps }) => {
+        const { checked, ...props } = getToggleAllPageRowsSelectedProps();
+        return (
+          <Box>
+            <Checkbox {...props} isChecked={checked} />
+          </Box>
+        );
+      },
+      Cell: ({ row }: any) => {
+        const { checked, ...props } = row.getToggleRowSelectedProps();
+        return (
+          <Box>
+            <Checkbox {...props} isChecked={checked} />
+          </Box>
+        );
+      },
+      disableFilters: true,
+    },
+    {
+      Header: "Extention",
+      accessor: "extension",
+      Cell: ({ value }) => <Icon w="40px" h="auto" as={avatarExt(value)} />,
+      width: "40px",
+    },
+    {
+      Header: "Asset",
+      accessor: "asset",
+    },
+    {
+      Header: "Type",
+      accessor: "type",
+    },
+    {
+      Header: "URL",
+      accessor: "url",
+    },
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+  ];
+
+  //untuk menampung data yang diedit dan tambah data baru
+  // const [data, setData] = useState<DataItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [dataTampil, setDataTampil] = useState<any | null>([]);
+  const getTampil = async () => {
+    try {
+      // Panggil API menggunakan Axios dengan async/await
+      const response = await axiosCustom.get("/assets/not-used");
+
+      // Imitasi penundaan dengan setTimeout (ganti nilai 2000 dengan waktu yang Anda inginkan dalam milidetik)
+      const timer = setTimeout(() => {
+        setDataTampil(response.data.data);
+        // console.log(dataTampil);
+        setIsLoading(false); // Set isLoading to false to stop the spinner
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    } catch (error) {
+      console.error("Gagal memuat data:", error);
+      setIsLoading(false);
     }
+  };
 
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const [isModalNotif, setModalNotif] = useState(false);
-    const [message, setMessage] = useState("");
-    const [isError, setIsError] = useState(false);
-    const handleShowMessage = (msg: string, err: boolean) => {
-      setMessage(msg);
-      setIsError(err);
-      setModalNotif(true);
-    };
+  useEffect(() => {
+    // Panggil fungsi fetchData untuk memuat data
+    getTampil();
+    // Clear the timeout when the component is unmounted
+  }, []);
 
-    const hidenCols = [
-      "id", "url"
-    ];
+  const renderActions = (rowData: any) => {
+    return (
+      <>
+        <Link href={rowData?.url} target="_blank">
+          <Button
+            bgColor="teal.100"
+            _hover={{
+              bg: "teal.200",
+            }}
+            title="Lihat"
+            size="sm"
+          >
+            <ViewIcon />
+          </Button>
+        </Link>
+      </>
+    );
+  };
 
-    const avatarExt = (type:any) => {
-        switch (type) {
-          case "JPG" || "JPEG":
-            return BsFiletypeJpg;
-          case "PNG":
-            return BsFiletypePng;
-          case "GIF":
-            return BsFiletypeGif;
-          case "SVG":
-            return BsFiletypeSvg;
-          case "PDF":
-            return BsFiletypePdf;
-            default : return AiOutlineFileUnknown;
+  const [getCheckedRows, setGetCheckedRows] = useState<DataItem[]>([]);
+  let getSelectedRows: { id: string }[] = [];
+
+  const handleSelectedRowsChange = (newSelectedRows: DataItem[]) => {
+    if (newSelectedRows.length !== 0) {
+      getSelectedRows = newSelectedRows.flatMap((dataItem) => ({
+        id: dataItem.id,
+      }));
+    } else getSelectedRows = [];
+    // console.log(newSelectedRows);
+    // console.log(getSelectedRows);
+  };
+
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+  const deleteData = async () => {
+    try {
+      setIsLoadingDelete(true);
+      // Panggil API menggunakan Axios dengan async/await
+      const response = await axiosCustom.post(`/assets/bulk-delete`, {
+        asset: getSelectedRows,
+      });
+
+      // Imitasi penundaan dengan setTimeout (ganti nilai 2000 dengan waktu yang Anda inginkan dalam milidetik)
+      const timer = setTimeout(() => {
+        // console.log(response);
+        if (response.status === 200) {
+          setIsLoadingDelete(false);
+          handleShowMessage("Data berhasil dihapus.", false);
+          // setIsDeleteModalOpen(false);
+          getTampil();
         }
-    }
+      }, 1000);
 
-    const columns: ReadonlyArray<Column<DataItem>> = [
-      {
-        id: "selection",
-        Header: ({ getToggleAllPageRowsSelectedProps }) => {
-          const { checked, ...props } = getToggleAllPageRowsSelectedProps();
-          return (
-            <Box>
-              <Checkbox {...props} isChecked={checked} />
-            </Box>
-          );
-        },
-        Cell: ({ row }: any) => {
-          const { checked, ...props } = row.getToggleRowSelectedProps();
-          return (
-            <Box>
-              <Checkbox {...props} isChecked={checked} />
-            </Box>
-          );
-        },
-        disableFilters: true,
-      },
-      {
-        Header: "Extention",
-        accessor: "extension",
-        Cell: ({ value }) => <Icon w="40px" h="auto" as={avatarExt(value)} />,
-        width: "40px",
-      },
-      {
-        Header: "Nama Asset",
-        accessor: "asset",
-      },
-      {
-        Header: "Type",
-        accessor: "type",
-      },
-      {
-        Header: "URL",
-        accessor: "url",
-      },
-      {
-        Header: "ID",
-        accessor: "id",
-      },
-    ];
-
-    //untuk menampung data yang diedit dan tambah data baru
-    // const [data, setData] = useState<DataItem[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    const [dataTampil, setDataTampil] = useState<any | null>([]);
-    const getTampil = async () => {
-      try {
-        // Panggil API menggunakan Axios dengan async/await
-        const response = await axiosCustom.get("/assets/not-used");
-
-        // Imitasi penundaan dengan setTimeout (ganti nilai 2000 dengan waktu yang Anda inginkan dalam milidetik)
-        const timer = setTimeout(() => {
-          setDataTampil(response.data.data);
-          // console.log(dataTampil);
-          setIsLoading(false); // Set isLoading to false to stop the spinner
-        }, 1000);
-
-        return () => clearTimeout(timer);
-      } catch (error) {
-        console.error("Gagal memuat data:", error);
-        setIsLoading(false);
-      }
-    };
-
-    useEffect(() => {
-      // Panggil fungsi fetchData untuk memuat data
-      getTampil();
-      // Clear the timeout when the component is unmounted
-    }, []);
-
-    const renderActions = (rowData: any) => {
-      return (
-        <>
-            <Button
-              bgColor="teal.100"
-              _hover={{
-                bg: "teal.200",
-              }}
-              // color="white"
-              title="Edit Data"
-            //   onClick={() => handleEdit(rowData)}
-              key="editData"
-              size="sm"
-            >
-              <ViewIcon />
-            </Button>
-        </>
-      );
-    };
-
-    const [getCheckedRows, setGetCheckedRows] = useState<DataItem[]>([]);
-    let getSelectedRows: { id: string; }[] = [];
-
-    const handleSelectedRowsChange = (newSelectedRows: DataItem[]) => {
-      if (newSelectedRows.length !== 0) {
-        getSelectedRows = newSelectedRows.flatMap((dataItem) => ({
-          id: dataItem.id,
-        }));
-      } else getSelectedRows = [];
-      // console.log(newSelectedRows);
-      // console.log(getSelectedRows);
-    };
-
-    const [isLoadingDelete, setIsLoadingDelete] = useState(false);
-    const deleteData = async() => {
-      try {
-        setIsLoadingDelete(true);
-        // Panggil API menggunakan Axios dengan async/await
-        const response = await axiosCustom.delete(`/asset/bulk-delete`, {
-          data: {
-            asset: getSelectedRows,
-          },
-        });
-
-        // Imitasi penundaan dengan setTimeout (ganti nilai 2000 dengan waktu yang Anda inginkan dalam milidetik)
-        const timer = setTimeout(() => {
-          // console.log(response);
-          if (response.status === 200) {
-            setIsLoadingDelete(false);
-            handleShowMessage("Data berhasil dihapus.", false);
-            // setIsDeleteModalOpen(false);
-            getTampil();
-          }
-        }, 1000);
-
-        return () => clearTimeout(timer);
-      } catch (error: any) {
-        if (error?.response) {
-          handleShowMessage(
-            `Terjadi Kesalahan: ${error.response.data.message}`,
-            true,
-          );
-        } else handleShowMessage(`Terjadi Kesalahan: ${error.message}`, true);
-        setIsLoadingDelete(false);
-      }
-    }
-    const handleDelete = () => {
-      if (getSelectedRows.length !== 0) {
-        setIsDeleteModalOpen(true);
-      } else return handleShowMessage(
-          `Terjadi Kesalahan: Pilih terlebih dahulu asset yang ingin dihapus!.`,
+      return () => clearTimeout(timer);
+    } catch (error: any) {
+      if (error?.response) {
+        handleShowMessage(
+          `Terjadi Kesalahan: ${error.response.data.message}`,
           true,
         );
+      } else handleShowMessage(`Terjadi Kesalahan: ${error.message}`, true);
+      setIsLoadingDelete(false);
     }
+  };
+  const handleDelete = () => {
+    if (getSelectedRows.length !== 0) {
+      setIsDeleteModalOpen(true);
+    } else
+      return handleShowMessage(
+        `Terjadi Kesalahan: Pilih terlebih dahulu asset yang ingin dihapus!.`,
+        true,
+      );
+  };
 
   return (
     <div>
@@ -270,7 +273,7 @@ function page() {
             pb="2"
             direction={["column", "row"]}
           >
-            <Heading fontSize={"2xl"}>DAFTAR TENANT</Heading>
+            <Heading fontSize={"2xl"}>ASSET TIDAK DIPAKAI</Heading>
 
             <HStack>
               <Button
