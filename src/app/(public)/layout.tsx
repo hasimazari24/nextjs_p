99a76f103c0668d71e957613af48b7727fd96f9d
yfbahtiar <yusuf2bahtiar@gmail.com>
2023-11-02 18:12:03 +0700

    fix: Cannot find module './utils/PublicContext'

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,10 +2,8 @@
 
 import Navbar from "./template/Navbar";
 import Footer from "./template/Footer";
-import type { Metadata } from "next";
 import { usePathname } from "next/navigation";
-import { PublicProvider } from "./utils/PublicContext";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 // import { Suspense } from "react";
 // import Loading from "./loading";
 
@@ -19,20 +17,20 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       {/* <Box w="full"> */}
-        {pathname === "/login" ? (
-          <>{children}</>
-        ) : (
-          <div>
-            <Box display="flex" minH="100vh" w="full" flexDirection={"column"}>
-              <Navbar />
-              <Box flex="1">{children}</Box>
-              <Box as="footer" w="full">
-                {/* Footer Anda di sini */}
-                <Footer />
-              </Box>
+      {pathname === "/login" ? (
+        <>{children}</>
+      ) : (
+        <div>
+          <Box display="flex" minH="100vh" w="full" flexDirection={"column"}>
+            <Navbar />
+            <Box flex="1">{children}</Box>
+            <Box as="footer" w="full">
+              {/* Footer Anda di sini */}
+              <Footer />
             </Box>
-          </div>
-        )}
+          </Box>
+        </div>
+      )}
 
       {/* </Box> */}
     </main>
