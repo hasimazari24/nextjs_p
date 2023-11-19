1d5a9f4366a0d2febf80c06f82c654aa03041ded
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-11-19 21:35:29 +0700

    remove textcolor plugins on tiny

--- src/app/type/inital-rich-text.ts
+++ src/app/type/inital-rich-text.ts
@@ -19,7 +19,7 @@ const initRichTextProps = {
     "table",
     "help",
     "wordcount",
-    "textcolor",
+    // "textcolor",
   ],
   toolbar:
     "fullscreen preview | undo redo | bold italic|forecolor|" +
