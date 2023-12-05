153b0ce61acac985708e4ebe254ca2fe54a4868d
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-12-05 11:26:18 +0700

    add detail group pertanyaan with react-dnd n preview page

--- src/app/(secure)/kuesioner/grup/[id_group]/page.tsx
+++ src/app/(secure)/kuesioner/grup/[id_group]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
-import React, { useState, useEffect, Suspense } from "react";
+import { DropResult } from "react-beautiful-dnd";
+import React, { useEffect, useState, Suspense } from "react";
+import { Column } from "react-table";
+import DndTable from "@/app/components/datatable/DndTable";
 import {
   Stack,
   Flex,
@@ -18,6 +21,10 @@ import { FindDefaultRoute } from "@/app/components/utils/FindDefaultRoute";
 import { useRouter } from "next/navigation";
 import { HiFolderOpen } from "react-icons/hi";
 import { ViewIcon } from "@chakra-ui/icons";
+interface Cards {
+  id: string;
+  title: string;
+}
 
 function page({ params }: { params: { id_group: string } }) {
   const { setBreadcrumbs } = useBreadcrumbContext();
@@ -26,6 +33,66 @@ function page({ params }: { params: { id_group: string } }) {
   //     if (getForCrumbs) setBreadcrumbs(getForCrumbs);
   //   }, []);
   const router = useRouter();
+
+  const [data, setData] = useState<Cards[] | []>([]);
+  const handleDragEnd = async (result: DropResult) => {
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.index === source.index) {
+      return;
+    }
+
+    // Update the order in your state (or wherever you keep it)
+    const newData = Array.from(data);
+    const [removed] = newData.splice(source.index, 1);
+    newData.splice(destination.index, 0, removed);
+    setData([...newData]);
+    const updatedData = newData.map((item, index) => {
+      return { ...item, no_urut: index + 1 };
+    });
+    console.log("data : ", data);
+    console.log("updatedData : ", updatedData);
+  };
+
+  const columns: ReadonlyArray<Column<Cards>> = [
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+    {
+      Header: "Judul",
+      accessor: "title",
+    },
+  ];
+
+  useEffect(() => {
+    setData([
+      {
+        id: "656aac72bf7b36c645595886",
+        title: "Dean Mclaughlin",
+      },
+      {
+        id: "656aac7245358b8608c5f028",
+        title: "Brock Compton",
+      },
+      {
+        id: "656aac72ac9ae63994e686bd",
+        title: "Harvey Rodriguez",
+      },
+      {
+        id: "656aac7234c589aef01bf4a3",
+        title: "Connie Hill",
+      },
+      {
+        id: "656aac722a9a5778cf140483",
+        title: "Jarvis Maldonado",
+      },
+    ]);
+  }, []);
 
   return (
     <Suspense fallback={<Loading />}>
@@ -95,10 +162,16 @@ function page({ params }: { params: { id_group: string } }) {
           borderColor={"blue.500"}
           w="full"
         >
-          <Stack justifyContent={"center"} alignItems={"center"}>
+          <Stack justifyContent={"center"} alignItems={"center"} spacing={3}>
             <Text fontWeight={"bold"} fontSize={["md", "lg"]}>
               DAFTAR PERTANYAAN :
             </Text>
+            <DndTable
+              columns={columns}
+              data={data}
+              droppableId={"123"}
+              onDragEnd={(result) => handleDragEnd(result)}
+            />
           </Stack>
         </Box>
       </Stack>
