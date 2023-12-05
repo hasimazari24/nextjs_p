ba2a9b8a22504ed2aed6cbee405a1c11a5b0dff4
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-12-05 14:51:41 +0700

    fix assets breadcrumbs

--- src/app/(secure)/assets/page.tsx
+++ src/app/(secure)/assets/page.tsx
@@ -1,18 +1,22 @@
-import React, { Suspense } from "react";
+"use client";
+import React, { useEffect } from "react";
 import dynamic from "next/dynamic";
 import Loading from "./loading";
+import { useBreadcrumbContext } from "@/app/components/utils/BreadCrumbsContext";
+import { FindDefaultRoute } from "@/app/components/utils/FindDefaultRoute";
 
 const Assets = dynamic(() => import("./getAssets"), {
   ssr: false,
-  loading: ()=> <Loading />
+  loading: () => <Loading />,
 });
 
 function page() {
-  return (
-    // <Suspense fallback={<Loading />}>
-      <Assets />
-    // </Suspense>
-  );
+  const { setBreadcrumbs } = useBreadcrumbContext();
+  const getForCrumbs: any = FindDefaultRoute();
+  useEffect(() => {
+    if (getForCrumbs) setBreadcrumbs(getForCrumbs);
+  }, []);
+  return <Assets />;
 }
 
 export default page;
