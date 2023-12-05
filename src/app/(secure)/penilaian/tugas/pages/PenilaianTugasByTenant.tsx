8f69ca5efc666ce2dd659c222ee9d5612c6b5e56
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-12-05 11:27:08 +0700

    fix random uuid at penilaiantugas by tenant

--- src/app/(secure)/penilaian/tugas/pages/PenilaianTugasByTenant.tsx
+++ src/app/(secure)/penilaian/tugas/pages/PenilaianTugasByTenant.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
 import ListKelas from "../[id_tenant]/page";
+import crypto from 'crypto';
 
 function PenilaianTugasByTenant() {
+  // const crypto = require("crypto");
+  const randomID = crypto.randomUUID();
   return (
     <div>
-      <ListKelas params={{ id_tenant : crypto.randomUUID() }} />
+      <ListKelas params={{ id_tenant: randomID }} />
     </div>
-  )
+  );
 }
 
 export default PenilaianTugasByTenant
