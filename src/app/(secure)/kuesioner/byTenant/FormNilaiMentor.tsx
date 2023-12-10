cc86ee14ebfe2baed7d301d42c786d6137da4586
hasimazari24 <202021050u@mhs.udb.ac.id>
2023-12-10 18:30:07 +0700

    add send kuesioner nilai mentor in modal

---
+++ src/app/(secure)/kuesioner/byTenant/FormNilaiMentor.tsx
@@ -0,0 +1,378 @@
+"use client";
+import React, { useState } from "react";
+import { HiFolderOpen } from "react-icons/hi";
+import {
+  HStack,
+  Icon,
+  Stack,
+  Text,
+  Center,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormErrorMessage,
+  Button,
+  Hide,
+  Input,
+  Textarea,
+  Checkbox,
+  CheckboxGroup,
+  Box,
+} from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { InfoIcon } from "@chakra-ui/icons";
+import ModalNotif from "@/app/components/modal/modal-notif";
+import { RiSendPlaneFill } from "react-icons/ri";
+import { axiosCustom } from "@/app/api/axios";
+
+interface PertanyaanProps {
+  id: string;
+  pertanyaan: string;
+  type: string; // Add other types if needed
+  is_required: boolean;
+  note: string;
+  opsi: OpsiProps;
+}
+
+interface OpsiProps {
+  id: string;
+  value: string;
+}
+
+interface SubmitAnswer {
+  id_pertanyaan: string;
+  opsi_submit: OpsiProps[];
+  text_submit: string;
+}
+
+interface FormData {
+  data: SubmitAnswer[];
+}
+
+function FormNilaiMentor({
+  dataPertanyaan,
+  isSubmitted,
+  onSubmit,
+  idKuesioner,
+}: {
+  dataPertanyaan: PertanyaanProps[] | [];
+  isSubmitted?: boolean;
+  onSubmit?: () => void;
+  idKuesioner?: string;
+}) {
+  const {
+    // control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [isDone, setIsDone] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [stateNotif, setStateNotif] = useState({
+    msg: "",
+    isError: false,
+    isNotifShow: false,
+  });
+  const handleShowMessage = (msg: string, err: boolean) => {
+    setStateNotif({
+      msg: msg,
+      isError: err,
+      isNotifShow: true,
+    });
+  };
+
+  const handleFormSubmit: SubmitHandler<FormData> = async (data) => {
+    // console.log(data);
+    try {
+      setIsLoading(true);
+      // Simpan data menggunakan Axios POST atau PUT request, tergantung pada mode tambah/edit
+      await axiosCustom
+        .post(`/kuesioner/${idKuesioner}/nilai-mentor`, data)
+        .then((response) => {
+          // console.log(response);
+          if (response.status === 201) {
+            handleShowMessage("Kuesioner berhasil dikirim.", false);
+            setIsLoading(false);
+          }
+        });
+    } catch (error: any) {
+      // console.error(error);
+      if (error?.response) {
+        handleShowMessage(
+          `Terjadi Kesalahan: ${error.response.data.message}`,
+          true,
+        );
+      } else handleShowMessage(`Terjadi Kesalahan: ${error.message}`, true);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Stack w="full">
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        {dataPertanyaan.map((que, index) => (
+          <Stack
+            w="full"
+            spacing={3}
+            p={{ base: 2, md: 4 }}
+            rounded={["md", "lg"]}
+            borderWidth={"4px"}
+            borderColor={"purple.500"}
+            key={que.id}
+            mb={4}
+          >
+            <Hide>
+              <Input
+                {...register(`data.${index}.id_pertanyaan`)}
+                defaultValue={que.id}
+              />
+            </Hide>
+            <HStack w="full" align="start" spacing={2}>
+              <Box
+                // p={1}
+                rounded={"md"}
+                bgColor={"purple.500"}
+                // boxSize={"30px"}
+                color={"white"}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Text
+                  w="25px"
+                  as={"b"}
+                  textAlign={"center"}
+                  whiteSpace={"nowrap"}
+                  mt="0.5"
+                >
+                  {index + 1}
+                </Text>
+              </Box>
+              <Stack spacing={4} key={que.id} mt="0.5">
+                {que.type === "radio" && (
+                  <FormControl
+                    mb={2}
+                    isInvalid={Boolean(errors.data?.[index]?.opsi_submit?.[0])}
+                  >
+                    <Text
+                      fontWeight={"bold"}
+                      fontSize={["15px", "16px"]}
+                      mb={1}
+                    >
+                      {que.pertanyaan}{" "}
+                      {que.is_required && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </Text>
+                    <Hide>
+                      <Input
+                        type="text"
+                        {...register(`data.${index}.id_pertanyaan`)}
+                        defaultValue={que.id}
+                      />
+                    </Hide>
+                    <RadioGroup>
+                      <Stack spacing={2}>
+                        {Array.isArray(que.opsi) &&
+                          que.opsi.length > 0 &&
+                          que.opsi.map((val) => (
+                            <Radio
+                              key={val.id}
+                              value={val.id}
+                              {...register(`data.${index}.opsi_submit.0`, {
+                                required: que.is_required
+                                  ? "Pertanyaan ini wajib dipilih."
+                                  : false,
+                              })}
+                            >
+                              {val.value}
+                            </Radio>
+                          ))}
+                      </Stack>
+                    </RadioGroup>
+                    <FormErrorMessage>
+                      {errors.data?.[index]?.opsi_submit?.[0]?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+                {que.type === "short_text" && (
+                  <FormControl
+                    mb={2}
+                    isInvalid={Boolean(errors.data?.[index]?.text_submit)}
+                  >
+                    <Text
+                      mb={1}
+                      fontWeight={"bold"}
+                      fontSize={["15px", "16px"]}
+                    >
+                      {que.pertanyaan}{" "}
+                      {que.is_required && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </Text>
+                    <Hide>
+                      <Input
+                        type="text"
+                        {...register(`data.${index}.id_pertanyaan`)}
+                        defaultValue={que.id}
+                      />
+                    </Hide>
+                    <Input
+                      type="text"
+                      {...register(`data.${index}.text_submit`, {
+                        required: que.is_required
+                          ? "Pertanyaan ini wajib diisi."
+                          : false,
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.data?.[index]?.text_submit?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+                {que.type === "long_text" && (
+                  <FormControl
+                    mb={2}
+                    isInvalid={Boolean(errors.data?.[index]?.text_submit)}
+                  >
+                    <Text
+                      mb={1}
+                      fontWeight={"bold"}
+                      fontSize={["15px", "16px"]}
+                    >
+                      {que.pertanyaan}{" "}
+                      {que.is_required && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </Text>
+                    <Hide>
+                      <Input
+                        type="text"
+                        {...register(`data.${index}.id_pertanyaan`)}
+                        defaultValue={que.id}
+                      />
+                    </Hide>
+                    <Textarea
+                      {...register(`data.${index}.text_submit`, {
+                        required: que.is_required
+                          ? "Pertanyaan ini wajib diisi."
+                          : false,
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.data?.[index]?.text_submit?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+                {que.type === "checkbox" && (
+                  <FormControl
+                    mb={2}
+                    isInvalid={Boolean(errors.data?.[index]?.opsi_submit)}
+                  >
+                    <Text
+                      mb={1}
+                      fontWeight={"bold"}
+                      fontSize={["15px", "16px"]}
+                    >
+                      {que.pertanyaan}{" "}
+                      {que.is_required && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </Text>
+                    <Hide>
+                      <Input
+                        type="text"
+                        {...register(`data.${index}.id_pertanyaan`)}
+                        defaultValue={que.id}
+                      />
+                    </Hide>
+                    <CheckboxGroup>
+                      <Stack spacing={2}>
+                        {Array.isArray(que.opsi) &&
+                          que.opsi.length > 0 &&
+                          que.opsi.map((val) => (
+                            <Checkbox
+                              key={val.id}
+                              value={val.id}
+                              {...register(`data.${index}.opsi_submit`, {
+                                required: que.is_required
+                                  ? "Pertanyaan ini wajib dipilih."
+                                  : false,
+                              })}
+                            >
+                              {val.value}
+                            </Checkbox>
+                          ))}
+                      </Stack>
+                    </CheckboxGroup>
+
+                    <FormErrorMessage>
+                      {errors.data?.[index]?.opsi_submit?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+              </Stack>
+            </HStack>
+          </Stack>
+        ))}
+        {isSubmitted && (
+          <Stack alignItems={"center"} w="full" spacing={3} pb={5} pt={3}>
+            <Checkbox
+              onChange={(e) => setIsDone(e.target.checked)}
+              alignItems={"start"}
+              borderColor={"blue.400"}
+              alignContent={"justify"}
+            >
+              <Text mt={-1} textAlign={"justify"}>
+                Saya dengan ini menyatakan bahwa saya telah menilai mentor
+                tersebut dengan cermat dan obyektif selama periode mentoring
+                dalam inkubator bisnis Solo Technopark.
+              </Text>
+            </Checkbox>
+            <HStack
+              color="gray.600"
+              alignItems={"start"}
+              justifyContent={"center"}
+            >
+              <InfoIcon />
+              <Text fontSize={"15px"} mt={"-3px"}>
+                Kuesioner ini hanya bisa diisi satu kali, pastikan Anda sudah
+                mengisinya dengan benar
+              </Text>
+            </HStack>
+            <Button
+              colorScheme="blue"
+              type="submit"
+              size={"md"}
+              w="170px"
+              justifyContent={"start"}
+              isDisabled={!isDone}
+              isLoading={isLoading}
+            >
+              <RiSendPlaneFill fontSize={"18px"} />
+              &nbsp; Kirim Jawaban
+            </Button>
+          </Stack>
+        )}
+      </form>
+
+      <ModalNotif
+        isOpen={stateNotif.isNotifShow}
+        onClose={() =>
+          setStateNotif({
+            msg: "",
+            isError: false,
+            isNotifShow: false,
+          })
+        }
+        message={stateNotif.msg}
+        isError={stateNotif.isError}
+        onSubmit={() => (onSubmit ? onSubmit() : null)}
+      />
+    </Stack>
+  );
+}
+
+export default FormNilaiMentor;
---
+++ src/app/(secure)/kuesioner/byTenant/TenantNilaiMentor.tsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Text,
+  Heading,
+  Stack,
+  Box,
+  Image,
+  SimpleGrid,
+  Center,
+} from "@chakra-ui/react";
+import FormKuesioner from "../kelola/[id_kuesioner]/components/FormKuesioner";
+import { axiosCustom } from "@/app/api/axios";
+import { ViewIcon } from "@chakra-ui/icons";
+import Loading from "../loading";
+import FormNilaiMentor from "./FormNilaiMentor";
+import ProfileMentor from "../../kelas/pages/profileMentor";
+
+const TenantNilaiMentor = ({
+  idKuesioner,
+  isOpen,
+  onClose,
+  onSubmit,
+}: {
+  idKuesioner: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+}) => {
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = useState(true);
+  const InitalState: {
+    tenantNilaiMentor: any | null;
+  } = {
+    tenantNilaiMentor: null,
+  };
+  const [state, setState] = useState(InitalState);
+
+  useEffect(() => {
+    const getAll = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axiosCustom.get(
+          `/kuesioner/${idKuesioner}/nilai-mentor`,
+        );
+        setState({
+          tenantNilaiMentor: response.data.data,
+        });
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setState({
+          tenantNilaiMentor: null,
+        });
+        setIsLoading(false);
+      }
+    };
+    if (isOpen === true) getAll();
+  }, [isOpen]);
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} size="full">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader mt={{ base: 5, md: 0 }}>
+            PENILAIAN MENTOR OLEH TENANT
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {isLoading ? (
+              <Loading />
+            ) : (
+              //   state.tenantNilaiMentor && (
+              <Stack
+                justifyContent={"center"}
+                alignItems={"center"}
+                spacing={3}
+                w="full"
+                px={{ base: 0, md: "50px", lg: "100px", xl: "150px" }}
+              >
+                <Box
+                  p={{ base: 3, md: 6 }}
+                  rounded={["md", "lg"]}
+                  borderWidth={"4px"}
+                  borderColor={"purple.500"}
+                  bgColor={"purple.50"}
+                  w="full"
+                  boxShadow={"xl"}
+                  mb="6"
+                >
+                  <Stack spacing={3} w="full">
+                    <Text
+                      fontWeight={"bold"}
+                      fontSize={["lg", "xl"]}
+                      textAlign={"center"}
+                      w="full"
+                      bgColor={"purple.500"}
+                      color={"white"}
+                      py={2}
+                    >
+                      IDENTITAS MENTOR :
+                    </Text>
+                    <SimpleGrid columns={{ base: 1, md: 3 }} spacing={2}>
+                      <Box
+                        alignItems={"center"}
+                        textAlign={"center"}
+                        borderRight={{ md: "3px solid" }}
+                        borderBottom={{ base: "2px solid", md: "none" }}
+                        borderColor={"purple.500"}
+                        pb={2}
+                        pr={{ base: 0, md: 3 }}
+                      >
+                        <Text fontSize={["md", "lg"]}>Kelas Mentoring</Text>
+                        <Text fontSize={["lg", "xl"]} as="b">
+                          {state.tenantNilaiMentor?.course?.name}
+                        </Text>
+                      </Box>
+                      <Box
+                        textAlign={"center"}
+                        borderBottom={{ base: "2px solid", md: "none" }}
+                        borderColor={"purple.500"}
+                        pb={2}
+                      >
+                        <Text fontSize={["md", "lg"]}>Mentor</Text>
+                        <Center>
+                          <ProfileMentor
+                            mentor={state.tenantNilaiMentor?.mentor}
+                          />
+                        </Center>
+                      </Box>
+                      <Box
+                        textAlign={"center"}
+                        borderLeft={{ md: "3px solid" }}
+                        borderColor={"purple.500"}
+                      >
+                        <Text fontSize={["md", "lg"]}>Kelas Diakhiri</Text>
+                        <Text fontSize={["lg", "xl"]} as="b">
+                          25 Oktober 2023
+                        </Text>
+                      </Box>
+                    </SimpleGrid>
+                  </Stack>
+                </Box>
+                <FormNilaiMentor
+                  dataPertanyaan={
+                    Array.isArray(state.tenantNilaiMentor?.pertanyaan)
+                      ? state.tenantNilaiMentor?.pertanyaan
+                      : []
+                  }
+                  isSubmitted={true}
+                  onSubmit={() => {
+                    onSubmit();
+                    onClose();
+                  }}
+                />
+              </Stack>
+              //   )
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default TenantNilaiMentor;
